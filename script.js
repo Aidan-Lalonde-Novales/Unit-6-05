e521b9fc94cb2b9dcf23404d596e94ed25f7a243
Aidan Lalonde-Novales <aidan.lalonde-novales@ocsbstudent.ca>
2021-01-06 15:17:38 +0000

    AND LOLER

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function textboxChecker () {
   const userDay = document.getElementById('userday').value // Makes the value of the day of the week textbox into a variable.
   if (userDay === 'Saturday' || userDay === 'Sunday') { // Checks to see if it a weekend.
     document.getElementById('result').innerHTML = 'It is a Weekend.' // If it is a weekend, this will display.
-  } else if ((userDay !== 'Sunday') || (userDay !== 'Saturday') && (userAge >= 18)) { // Checks to see if the user's age is above or equal to 18.
+  } else if ((userDay !== 'Sunday') && (userDay !== 'Saturday') && (userAge >= 18)) { // Checks to see if the user's age is above or equal to 18.
     document.getElementById('result').innerHTML = 'It is a Work Day.' // If user age is 18 or above, this will display.
-  } else if ((userDay !== 'Sunday') || (userDay !== 'Saturday') && (userAge <= 17)) { // Checks to see if the user's age is under or equal 17.
+  } else if ((userDay !== 'Sunday') && (userDay !== 'Saturday') && (userAge <= 17)) { // Checks to see if the user's age is under or equal 17.
     document.getElementById('result').innerHTML = 'It is a School Day.' // If the user is under 18, this will display.
   } else {
     document.getElementById('result').innerHTML = 'Please enter valid information.' // This will display if invalid information is presented.
